df2170d37045bfd20030e1b37ce8b8e814557c9a
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2023-12-24 14:56:18 +0100

    Day03 Part 2 Solution

--- src/day03.rs
+++ src/day03.rs
@@ -1,5 +1,7 @@
 // https://adventofcode.com/2023/day/3
 
+use std::collections::HashMap;
+
 pub fn solve(input: String) {
     println!("Day 3 solutions: ");
     solve_part_1(&input);
@@ -23,7 +25,7 @@ pub fn solve_part_1(input: &String) {
             }
 
             if !part_number {
-                part_number = is_part_number(input, line_number, char_index);
+                part_number = is_part_number(&input, line_number, char_index, is_part_symbol);
             }
 
             let number_finished = match line.chars().nth(char_index + 1) {
@@ -50,10 +52,89 @@ pub fn solve_part_1(input: &String) {
 }
 
 pub fn solve_part_2(input: &String) {
-    println!("\t Part 2: {}", input.len());
+    let mut gear_map: HashMap<(usize, usize), Vec<i32>> = HashMap::new();
+
+    for (line_number, line) in input.lines().enumerate() {
+        let mut begin_index = Option::None;
+        let mut part_coords = Option::None;
+
+        for (char_index, c) in line.chars().enumerate() {
+            if !c.is_digit(10) {
+                continue;
+            }
+
+            if begin_index.is_none() {
+                begin_index = Some(char_index);
+            }
+
+            if part_coords.is_none() {
+                part_coords = part_location(&input, line_number, char_index, is_gear);
+            }
+
+            let number_finished = match line.chars().nth(char_index + 1) {
+                Some(c) => !c.is_digit(10),
+                None => true,
+            };
+
+            if number_finished {
+                let num = line[begin_index.unwrap()..=char_index]
+                    .parse::<i32>()
+                    .unwrap();
+
+                if let Some(coords) = part_coords {
+                    match gear_map.get_mut(&coords) {
+                        Some(vec) => vec.push(num),
+                        None => {
+                            gear_map.insert(coords, vec![num]);
+                        }
+                    }
+                }
+
+                part_coords = Option::None;
+                begin_index = Option::None;
+            }
+        }
+    }
+
+    let mut sum = 0;
+    for (_, value) in gear_map.iter() {
+        if value.len() == 2 {
+            sum += value[0] * value[1];
+        }
+    }
+
+    println!("\t Part 2: {}", sum);
 }
 
-fn is_part_number(schematic: &str, line_index: usize, char_index: usize) -> bool {
+fn is_part_symbol(c: char) -> bool {
+    match c {
+        '*' | '/' | '+' | '-' | '%' | '=' | '@' | '#' | '&' | '$' => true,
+        _ => false,
+    }
+}
+
+fn is_gear(c: char) -> bool {
+    c == '*'
+}
+
+fn is_part_number(
+    schematic: &str,
+    line_index: usize,
+    char_index: usize,
+    match_symbol: fn(char) -> bool,
+) -> bool {
+    match part_location(schematic, line_index, char_index, match_symbol) {
+        Some(_) => true,
+        None => false,
+    }
+}
+
+fn part_location(
+    schematic: &str,
+    line_index: usize,
+    char_index: usize,
+    match_symbol: fn(char) -> bool,
+) -> Option<(usize, usize)> {
     // Clamp line indices between 0 and the number of lines
     let max_line = std::cmp::min(line_index + 1, schematic.lines().count());
     let min_line = match line_index {
@@ -85,12 +166,11 @@ fn is_part_number(schematic: &str, line_index: usize, char_index: usize) -> bool
                 None => continue,
             };
 
-            match c {
-                '*' | '/' | '+' | '-' | '%' | '=' | '@' | '#' | '&' | '$' => return true,
-                _ => continue,
+            if match_symbol(c) {
+                return Some((i, j));
             }
         }
     }
 
-    return false;
+    return None;
 }
