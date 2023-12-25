2965adbf28dc20baaf1e202f5bce975f0e2ad038
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2023-12-25 18:50:41 +0100

    Day04 Part 1 Solution

--- src/day04.rs
+++ src/day04.rs
@@ -1,5 +1,7 @@
 // https://adventofcode.com/2023/day/4
 
+use std::collections::HashSet;
+
 pub fn solve(input: String) {
     println!("Day 4 solutions: ");
     solve_part_1(&input);
@@ -7,11 +9,44 @@ pub fn solve(input: String) {
 }
 
 pub fn solve_part_1(input: &String) {
+    let mut points = 0;
+
+    for line in input.lines() {
+        let card = parse_card(line);
+        let matches = card.numbers.intersection(&card.winners).count();
+        if matches > 0 {
+            points += 2_i32.pow((matches - 1) as u32);
+        }
+    }
  
-    println!("\t Part 1: {}", input.len());
+    println!("\t Part 1: {}", points);
 }
 
 pub fn solve_part_2(input: &String) {
- 
-    println!("\t Part 2: {}", input.len());
+    
+    println!("\t Part 2: {}", 0);
+}
+
+#[derive(Debug)]
+struct Card {
+    winners: HashSet<i32>,
+    numbers: HashSet<i32>,
+}
+
+fn parse_card(line: &str) -> Card {
+    let mut all_numbers = line.split(": ").last().unwrap().split(" | ");
+    let winners = all_numbers.next().unwrap();
+    let numbers = all_numbers.next().unwrap();
+
+    let parse = |x: &str| -> HashSet<i32> {
+        x.split(" ")
+        .filter(|x| !x.is_empty())
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect()
+    };
+
+    Card {
+        winners: parse(winners),
+        numbers: parse(numbers),
+    }
 }
