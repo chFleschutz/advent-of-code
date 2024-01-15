76c93b871ecc3ac0717042d861d6adb221603cba
Christoph Fleschutz <ch.fleschutz@gmail.com>
2024-01-15 13:51:38 +0100

    Day05 Part 2 Solution

--- src/day05.rs
+++ src/day05.rs
@@ -38,12 +38,50 @@ pub fn solve_part_1(input: &String) {
 }
 
 pub fn solve_part_2(input: &String) {
-    println!("\t Part 2: {}", input.len());
+    let (
+        seeds,
+        seed_to_soil,
+        soil_to_fert,
+        fert_to_water,
+        water_to_ligth,
+        light_to_temp,
+        temp_to_humid,
+        humid_to_loc,
+    ) = parse_input(input);
+
+    let seed_ranges: Vec<Range> = seeds
+        .chunks(2)
+        .map(|chunk| Range {
+            start: chunk[0],
+            range: chunk[1],
+        })
+        .collect();
+
+    let soil_ranges = map_ranges(seed_ranges, &seed_to_soil);
+    let fert_ranges = map_ranges(soil_ranges, &soil_to_fert);
+    let water_ranges = map_ranges(fert_ranges, &fert_to_water);
+    let light_ranges = map_ranges(water_ranges, &water_to_ligth);
+    let temp_ranges = map_ranges(light_ranges, &light_to_temp);
+    let humid_ranges = map_ranges(temp_ranges, &temp_to_humid);
+    let loc_ranges = map_ranges(humid_ranges, &humid_to_loc);
+
+    let mut result = u32::MAX;
+    for range in loc_ranges {
+        result = std::cmp::min(result, range.start);
+    }
+
+    println!("\t Part 2: {}", result);
 }
 
 struct Mapping {
     destination: u32,
     source: u32,
+    range: u32,
+}
+
+#[derive(Clone)]
+struct Range {
+    start: u32,
     range: u32,
 }
 
@@ -130,5 +168,83 @@ fn map_value(value: u32, map: &Vec<Mapping>) -> u32 {
             break;
         }
     }
+    result
+}
+
+fn ranges_mapping(ranges: &Vec<Range>, mapping: &Mapping) -> (Vec<Range>, Vec<Range>) {
+    let mut map_ranges = Vec::new();
+    let mut new_ranges = Vec::new();
+
+    let mapping_max = match mapping.source.checked_add(mapping.range) {
+        Some(max) => max,
+        None => u32::MAX,
+    };
+
+    for range in ranges {
+        let range_max = match range.start.checked_add(range.range) {
+            Some(max) => max,
+            None => u32::MAX,
+        };
+
+        // No overlap
+        if range.start >= mapping_max || range_max <= mapping.source {
+            new_ranges.push(range.clone());
+            continue;
+        }
+
+        // Range is fully contained in mapping
+        if range.start >= mapping.source && range_max <= mapping_max {
+            map_ranges.push(Range {
+                start: mapping.destination + (range.start - mapping.source),
+                range: range.range,
+            });
+            continue;
+        }
+
+        let mut range_remaining = range.range;
+
+        // Range until mapping
+        if range.start < mapping.source {
+            new_ranges.push(Range {
+                start: range.start,
+                range: mapping.source - range.start,
+            });
+
+            range_remaining -= mapping.source - range.start;
+        }
+
+        // Range in mapping
+        let offset = match range.start > mapping.source {
+            true => range.start - mapping.source,
+            false => 0,
+        };
+        map_ranges.push(Range {
+            start: mapping.destination + offset,
+            range: std::cmp::min(std::cmp::min(mapping_max - range.start, range_remaining), mapping.range), 
+        });
+        
+        // Range after mapping
+        if range_max > mapping_max {
+            new_ranges.push(Range {
+                start: mapping_max,
+                range: range_max - mapping_max,
+            });
+        }
+    }
+
+    (new_ranges, map_ranges)
+}
+
+fn map_ranges(ranges: Vec<Range>, map: &Vec<Mapping>) -> Vec<Range> {
+    let mut result = Vec::new();
+    let mut new_ranges = ranges;
+
+    for mapping in map {
+        let (unmapped_ranges, mapped_ranges) = ranges_mapping(&new_ranges, mapping);
+        new_ranges = unmapped_ranges;
+        result.extend(mapped_ranges);
+    }
+
+    result.extend(new_ranges);
     result
 }
